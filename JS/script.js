08fe72a9717e4da7454ab8acdcf8f9579adcf4dc
Wulfazz <teito.klein@laposte.net>
2024-01-16 10:57:02 +0100

    updates img

--- JS/script.js
+++ JS/script.js
@@ -15,32 +15,50 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// Conteneur / bouttons nav
 const imageContainer = document.getElementById('image-container');
-const totalImages = 11; // Changez le nombre d'images selon vos besoins
-let currentIndex = 0;
+    const prevButton = document.getElementById('prev-btn');
+    const nextButton = document.getElementById('next-btn');
 
-// Génération des éléments HTML pour chaque image
-for (let i = 1; i <= totalImages; i++) {
-    const imgElement = document.createElement('img');
-    imgElement.src = `../img/carousel/img${i}.png`;
-    imgElement.alt = `Image ${i}`;
-    imgElement.className = 'carousel-image';
-    imgElement.addEventListener('click', () => showImage(i - 1)); // Soustrayez 1 pour l'index correct
-    imageContainer.appendChild(imgElement);
-}
+    // Nombre d'images
+    const totalImages = 11;
+    let currentIndex = 0;
 
-// Fonction pour afficher une image spécifique
-function showImage(index) {
-    currentIndex = index;
-    const translateValue = -index * 100 + '%';
-    imageContainer.style.transform = `translateX(${translateValue})`;
-}
+    // Génération des éléments HTML pour chaque image
+    for (let i = 1; i <= totalImages; i++) {
+        const imgElement = document.createElement('img');
+        // Source des images carousel
+        imgElement.src = `../img/carousel/img${i}.png`;
+        // Ajout détail en cas de soucis d'affichage
+        imgElement.alt = `Image ${i}`;
+        // Ajout d'une classe
+        imgElement.className = 'carousel-image';
+        imgElement.addEventListener('click', () => showImage(i - 1));
+        imageContainer.appendChild(imgElement);
+    }
 
-// Fonction pour passer à l'image suivante
-function nextImage() {
-    currentIndex = (currentIndex + 1) % totalImages;
-    showImage(currentIndex);
-}
+    // Fonction pour affichage
+    function showImage(index) {
+        currentIndex = index;
+        const translateValue = -index * 100 + '%';
+        imageContainer.style.transform = `translateX(${translateValue})`;
+    }
 
-// Défilement automatique du carrousel
-setInterval(nextImage, 5000); // Changez la valeur pour ajuster la vitesse du défilement
+    // Fonction pour passer à l'image suivante
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % totalImages;
+        showImage(currentIndex);
+    }
+
+    // Fonction pour passer à l'image précédente
+    function prevImage() {
+        currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+        showImage(currentIndex);
+    }
+
+    // Gestion des événements pour les boutons de navigation
+    prevButton.addEventListener('click', prevImage);
+    nextButton.addEventListener('click', nextImage);
+
+    // Défilement automatique du carrousel
+    setInterval(nextImage, 4000); // Changez la valeur pour ajuster la vitesse du défilement
