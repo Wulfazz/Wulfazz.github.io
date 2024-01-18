d7527108174685c203e585764a0e2ee99c870621
Wulfazz <teito.klein@laposte.net>
2024-01-18 08:54:24 +0100

    update gallery

--- JS/script.js
+++ JS/script.js
@@ -15,50 +15,42 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// Conteneur / bouttons nav
-const imageContainer = document.getElementById('image-container');
-    const prevButton = document.getElementById('prev-btn');
-    const nextButton = document.getElementById('next-btn');
+document.addEventListener("DOMContentLoaded", function () {
+    const imageContainer = document.getElementById("image-container");
+    const prevBtn = document.getElementById("prev-btn");
+    const nextBtn = document.getElementById("next-btn");
 
-    // Nombre d'images
-    const totalImages = 16;
     let currentIndex = 0;
+    let totalImages = 16;  // Remplacez par le nombre total d'images
+    let imagesPath = "../img/carousel/img";
 
     // Génération des éléments HTML pour chaque image
     for (let i = 1; i <= totalImages; i++) {
-        const imgElement = document.createElement('img');
-        // Source des images carousel
-        imgElement.src = `../img/carousel/img${i}.png`;
-        // Ajout détail en cas de soucis d'affichage
-        imgElement.alt = `Image ${i}`;
-        // Ajout d'une classe
+        const imgElement = document.createElement('div');
+        imgElement.style.backgroundImage = `url('${imagesPath + i}.png')`;
         imgElement.className = 'carousel-image';
         imgElement.addEventListener('click', () => showImage(i - 1));
         imageContainer.appendChild(imgElement);
     }
 
-    // Fonction pour affichage
     function showImage(index) {
         currentIndex = index;
         const translateValue = -index * 100 + '%';
         imageContainer.style.transform = `translateX(${translateValue})`;
     }
 
-    // Fonction pour passer à l'image suivante
     function nextImage() {
         currentIndex = (currentIndex + 1) % totalImages;
         showImage(currentIndex);
     }
 
-    // Fonction pour passer à l'image précédente
     function prevImage() {
         currentIndex = (currentIndex - 1 + totalImages) % totalImages;
         showImage(currentIndex);
     }
 
-    // Gestion des événements pour les boutons de navigation
-    prevButton.addEventListener('click', prevImage);
-    nextButton.addEventListener('click', nextImage);
+    prevBtn.addEventListener('click', prevImage);
+    nextBtn.addEventListener('click', nextImage);
 
-    // Défilement automatique du carrousel
-    setInterval(nextImage, 5000); // Changez la valeur pour ajuster la vitesse du défilement
+    showImage(currentIndex);
+});
